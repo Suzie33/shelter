4dadcca765842e5d32c15c116dba81ae2c359f65
Valentina Ponamareva <valiazin@gmail.com>
2020-09-28 23:26:02 +0300

    feat: add styles for header

--- shelter/gulpfile.js
+++ shelter/gulpfile.js
@@ -1,14 +1,36 @@
 const { src, dest, task, series, watch, parallel } = require("gulp");
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 task('sass', function(){
   return src('css/styles.scss')
     .pipe(sassGlob())
     .pipe(sass())
-    .pipe(dest('./css'));
+    .pipe(dest('./css'))
+    .pipe(reload({ stream: true }));
 });
 
-task('watch', function(){
-  return watch('css/**/*.scss', series('sass'));
+task('html', () => {
+  return src('pages/**/*.html')
+    .pipe(reload({ stream: true }));
+})
+
+task('server', () => {
+  browserSync.init({
+     server: {
+           baseDir: "./"
+     },
+     open: false
+ });
 });
+
+task('watch', () => {
+  watch('css/**/*.scss', series('sass'));
+  watch('pages/**/*.html', series('html'))
+});
+
+task('default', 
+  series(parallel('watch', 'server'))
+);
