e23b86b5b0ec4172d22eba463d18d4852b7fe24f
Valentina Ponamareva <valiazin@gmail.com>
2020-10-26 16:56:57 +0300

    feat: implement burger menu pets page

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -4,40 +4,35 @@ const hamburgerNav = document.querySelector('#nav-hamburger');
 const mainLogo = document.querySelector('#main-logo');
 const overlay = document.querySelector('#overlay');
 const activeMenuElement = hamburgerNav.querySelector('.nav__item--active');
+const headerFixed = document.querySelector('.header--fixed');
+const main = document.querySelector('#main');
 
-hamburgerMenuBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+hamburgerMenuBtn.addEventListener('click', () => {
 
   if (hamburgerNav.classList.contains('nav--hamburger-visible')) {
     hamburgerNav.classList.remove('nav--hamburger-visible');
     hamburgerMenuBtn.classList.remove('hamburger-menu-link--hamburger');
-    mainLogo.style.visibility = 'visible';
+    mainLogo.style.display = 'block';
     document.body.style.overflow = 'visible';
     overlay.style.display = 'none';
+    headerFixed.classList.remove('header--fixed--static');
+    main.classList.add('main--padding--top');
+
   } else {
     hamburgerNav.classList.add('nav--hamburger-visible');
     hamburgerMenuBtn.classList.add('hamburger-menu-link--hamburger');
-    mainLogo.style.visibility = 'hidden';
+    mainLogo.style.display = 'none';
     document.body.style.overflow = 'hidden';
     overlay.style.display = 'block';
+    headerFixed.classList.add('header--fixed--static');
+    main.classList.remove('main--padding--top');
   }
 })
 
 overlay.addEventListener('click', () => {
-  hamburgerNav.classList.remove('nav--hamburger-visible');
-  hamburgerMenuBtn.classList.remove('hamburger-menu-link--hamburger');
-  mainLogo.style.visibility = 'visible';
-  document.body.style.overflow = 'visible';
-  overlay.style.display = 'none';
+  hamburgerMenuBtn.click();
 })
-console.log(activeMenuElement);
 
 activeMenuElement.addEventListener('click', () => {
-  if (hamburgerNav.classList.contains('nav--hamburger-visible')) {
-    hamburgerNav.classList.remove('nav--hamburger-visible');
-    hamburgerMenuBtn.classList.remove('hamburger-menu-link--hamburger');
-    mainLogo.style.visibility = 'visible';
-    document.body.style.overflow = 'visible';
-    overlay.style.display = 'none';
-  }
+  hamburgerMenuBtn.click();
 })
